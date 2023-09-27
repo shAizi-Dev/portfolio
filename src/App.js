e10137b342ee58ac542f2675d6bbfaeb6f258eab
shahroz <shahroimtiaz758@gmail.com>
2023-09-26 19:09:18 -0700

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-// import logo from './logo.svg';
 import './App.css';
+
 import Header from './Components/Header/Header'
 import Experience from './Pages/Experience';
 import Hero from './Pages/Hero';
 import Skills from './Pages/Skills';
 import Projects from './Pages/Projects'
 import Contact from './Pages/Contact'
-// import BackTop from './Components/BackTop';
-// import React {useState} from 'react'
 
 
 function App() {
