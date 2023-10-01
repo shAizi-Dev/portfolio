29dcb9bdf17b30014abad8c642afffaa779e739a
Muhammad Shahroz <112727838+shAizi-Dev@users.noreply.github.com>
2023-10-01 16:55:06 +0000

    commit change

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -48,7 +48,7 @@ function Contact() {
   )
 }
 const showAlert = () => {
-    alert('Discuss Any querry through E-mail => <br>shaizidev@gmail.com');
+    alert('Discuss Any querry through E-mail => shaizidev@gmail.com');
  };
 
 export default Contact
